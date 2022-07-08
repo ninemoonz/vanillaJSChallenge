66885e510cd90a1d1ca73661baedf46fd7d12dd0
ninemoonz <76804681+ninemoonz@users.noreply.github.com>
2022-07-08 18:44:22 +0900

    window resize event with if else statments

---
+++ vanillaJS_hw_02/app.js
@@ -0,0 +1,29 @@
+const body = document.querySelector("body");
+const widthOutput = document.querySelector("#width");
+const browserSize = window.innerWidth;
+
+widthOutput.textContent = window.innerWidth;
+
+function resizeEvent(){
+    widthOutput.textContent = window.innerWidth;
+    const browserSize = window.innerWidth;
+    if(browserSize >= 700 && browserSize <= 1100){
+        body.classList.add("default");
+        body.classList.remove("increased-width");
+        body.classList.remove("decreased-width");
+    }else if(browserSize > 1100){
+        body.classList.remove("default");
+        body.classList.remove("decreased-width");
+        body.classList.add("increased-width");
+    }else if(browserSize < 700){
+        body.classList.remove("default");       
+        body.classList.remove("increased-width");
+        body.classList.add("decreased-width");
+    }
+
+}
+
+resizeEvent();
+
+
+window.addEventListener("resize", resizeEvent);
